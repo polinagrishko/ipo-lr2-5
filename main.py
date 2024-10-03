c530714b965ff9c14b11996a21714d1707fdba1b
gfgfgfgf <pgrisko5@gmail.com>
2024-10-03 10:37:15 +0300

    ..

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-def time(n):
-    min = 24 * 60
-    
-    n = n % min
-    
-    hours = n // 60
-    min = n % 60
-    
-    return hours, min
-
-n = int(input("Введите количество минут с начала суток: "))
-
-hours, minutes = time(n)
-print(f"Часы: {hours}, Минуты: {min}")
+n = int(input("Введите количество минут прошедших с начала суток: "))
+minutes_in_day=1440
+n=n% minutes_in_day
+hours=n//60
+minutes = n%60
+print(f"Часы: {hours}, Минуты: {minutes}")
